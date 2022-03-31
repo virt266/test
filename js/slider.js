2d38d61d6813861c40fa1fcbaf53b34c47bbf791
Sergey Daynichenko <virt266@gmail.com>
2022-03-31 21:47:00 +0300

    done aside

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,19 @@
 (function(){
-	const slider = document.querySelectorAll('.fade-slider__item');
-	const activeClass = "fade-slider__item--visible";
-	let index = 0;
+	const controlls = document.querySelectorAll('.filter__link');
+	const activeClass = "filter__link--active";
 
-	setInterval(function() {
-		slider[index].classList.remove(activeClass);
-		index++;
-		if (index + 1 > slider.length){
-			index = 0;
-		}		
-		slider[index].classList.add(activeClass);
+	controlls.forEach(function(control){
 
-	},1000);
+		control.addEventListener('click', function(e) {
+		e.preventDefault(); 
+
+		controlls.forEach(function(link){
+			link.closest('.filter__item').classList.remove(activeClass);
+		})
+
+		control.closest('.filter__item').classList.add(activeClass);
+
+		})
+	})
 
 }())
