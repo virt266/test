c51c08867282632dec40e3d722e5cf0234c67e0a
Sergey Daynichenko <virt266@gmail.com>
2022-04-01 22:55:26 +0300

    done aside

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,12 @@
 (function(){
+
+	const elem = document.querySelector('.products__list');
+	const iso = new Isotope( elem, {
+	  // options
+	  itemSelector: '.products__item',
+	  filter: '.popular'
+	});
+
 	const controlls = document.querySelectorAll('.filter__link');
 	const activeClass = "filter__link--active";
 
@@ -6,13 +14,17 @@
 
 		control.addEventListener('click', function(e) {
 		e.preventDefault(); 
+		const filterName = control.getAttribute("data-filter")
 
 		controlls.forEach(function(link){
-			link.closest('.filter__item').classList.remove(activeClass);
+			link.closest('.filter__link').classList.remove(activeClass);
 		})
 
-		control.closest('.filter__item').classList.add(activeClass);
+		control.closest('.filter__link').classList.add(activeClass);
 
+		iso.arrange({
+			filter: `.${filterName}`
+		})
 		})
 	})
 
